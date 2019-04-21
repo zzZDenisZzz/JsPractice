ae9fc1de38209e118bcae2fc04b82e522dd44922
Denys Zazeka <denys.zazeka@gmail.com>
2019-04-21 13:30:48 +0300

    lesson Date

---
+++ js/Date.js
@@ -0,0 +1,9 @@
+//var now = new Date;
+//alert(now.getFullYear());
+//alert(now.getMonth());
+//alert(now.getMinutes());
+//alert(now.getUTCDate());
+
+var d = new Date();
+d.setDate(d.getDate()+5);
+alert(d);
